7edc0322012e1121c718296b542f5dfd29014d35
Almaz Shamilov <pepegapeepoclown4head@gmail.com>
2026-01-24 22:17:55 +0300

    add opensearch serializer based on System.Text.Json

--- src/server/ScanVul.Server.Infrastructure/OpenSearch/Entry.cs
+++ src/server/ScanVul.Server.Infrastructure/OpenSearch/Entry.cs
@@ -2,9 +2,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenSearch.Client;
-using OpenSearch.Client.JsonNetSerializer;
 using OpenSearch.Net;
 using ScanVul.Server.Domain.Cve.Repositories;
+using ScanVul.Server.Infrastructure.OpenSearch.Helpers;
 using ScanVul.Server.Infrastructure.OpenSearch.Repositories;
 using ScanVul.Server.Infrastructure.OpenSearch.Services;
 
@@ -23,7 +23,7 @@ public static class Entry
             throw new InvalidOperationException("OpenSearch settings not set");
 
         var pool = new SingleNodeConnectionPool(new Uri(options.Url));
-        var settings = new ConnectionSettings(pool, sourceSerializer: JsonNetSerializer.Default)
+        var settings = new ConnectionSettings(pool, sourceSerializer: SystemTextJsonSerializer.Default)
             .BasicAuthentication(options.Username, options.Password);
 
         if (environment.IsDevelopment())
